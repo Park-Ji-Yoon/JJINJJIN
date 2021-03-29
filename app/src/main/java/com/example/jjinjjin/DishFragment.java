adba4492423efd363986b6cb4d2393ed49dd9236
Park-Ji-Yoon <purun030515@gmail.com>
2021-03-30 00:28:20 +0900

    급식 api 연동, 학교/교육청 코드 api 연동

--- app/src/main/java/com/example/jjinjjin/DishFragment.java
+++ app/src/main/java/com/example/jjinjjin/DishFragment.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,11 +26,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DishFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DishFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
@@ -38,7 +35,10 @@ public class DishFragment extends Fragment {
     private String mParam2;
 
     TextView tvResult1;
+    TextView tvResult2;
     View view;
+    Button btnSchoolName;
+    EditText edSchoolName;
 
     public DishFragment() {
     }
@@ -66,31 +66,36 @@ public class DishFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_dish, container, false);
         tvResult1 = view.findViewById(R.id.tvResult1);
-        getStudentDish();
+        tvResult2 = view.findViewById(R.id.tvResult2);
+        btnSchoolName = view.findViewById(R.id.btnSchoolname);
+        edSchoolName = view.findViewById(R.id.edSchoolName);
+
+        btnSchoolName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(edSchoolName.getText() != null) {
+                    getSchoolCode();
+                }
+            }
+        });
+
         return view;
     }
 
-    public void getStudentDish(){
-        String tempUrl = "";
+    public void getStudentDish(String school_code, String school_edu){
         String nowtempUrl = "";
         String dish_url = "https://open.neis.go.kr/hub/mealServiceDietInfo";
-//        String city = etCity.getText().toString().trim();
-//        if (city.equals("")){
-//            tvResult1.setText("첫 번째 항목 : 필수 기입");
-//        }else{
+
         final String[] output = new String[2];
-//            nowtempUrl = nowurl + "?q=" + city + "&appid=" + appid;
-//            tempUrl = url + "?q=" + city + "&appid=" + appid;
 
         nowtempUrl = dish_url + "?Type=" + "json"
                 + "&pIndex=" + "1"
                 + "&pSize=" + "100"
-                + "&ATPT_OFCDC_SC_CODE=" + "T10"
-                + "&SD_SCHUL_CODE=" + "9296071"
+                + "&ATPT_OFCDC_SC_CODE=" + school_edu
+                + "&SD_SCHUL_CODE=" + school_code
                 + "&KEY=" + "7abd1b38b28943c0a64f7784516d3feb";
 
         StringRequest now_stringRequest = new StringRequest(Request.Method.POST, nowtempUrl, new Response.Listener<String>() {
-            @Nullable
             @Override
             public void onResponse(String response) {
                 try{
@@ -122,100 +127,41 @@ public class DishFragment extends Fragment {
                 Toast.makeText(getContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
             }
         });
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(now_stringRequest);
+//        requestQueue.add(stringRequest);
+    }
+
+    public String[] getSchoolCode(){
+        String nowtempUrl = "";
+        String dish_url = "https://open.neis.go.kr/hub/schoolInfo";
+        final String[] school_code = {"", ""};
+
+        nowtempUrl = dish_url + "?Type=" + "json"
+                + "&pIndex=" + "1"
+                + "&pSize=" + "100"
+                + "&KEY=" + "7c47d7824e0d4a0eb908d2c186de56f1"
+                + "&SCHUL_NM=" + edSchoolName.getText();
+
+        StringRequest now_stringRequest = new StringRequest(Request.Method.POST, nowtempUrl, new Response.Listener<String>() {
+            @Nullable
             @Override
             public void onResponse(String response) {
-                Log.d("response", response);
                 try{
-                    double temp_max = 0.0, temp_min = 0.0;
-                    int humidity = 0, clouds = 0;
-
                     JSONObject jsonResponse = new JSONObject(response);
-                    JSONArray jsonArray = jsonResponse.getJSONArray("list");
+                    JSONArray mealServiceDietInfo = jsonResponse.getJSONArray("schoolInfo");
+                    JSONObject body = mealServiceDietInfo.getJSONObject(1);
+                    JSONArray body_array = body.getJSONArray("row");
+                    JSONObject dish = body_array.getJSONObject(0);
+                    school_code[0] = dish.getString("ATPT_OFCDC_SC_CODE");
+                    school_code[1] = dish.getString("SD_SCHUL_CODE");
 
-                    JSONObject jsonObject_D0 = jsonArray.getJSONObject(0);
-                    JSONObject jsonObject_M0 = jsonObject_D0.getJSONObject("main");
-                    JSONObject jsonObject_D1 = jsonArray.getJSONObject(1);
-                    JSONObject jsonObject_M1 = jsonObject_D1.getJSONObject("main");
-                    JSONObject jsonObject_D2 = jsonArray.getJSONObject(2);
-                    JSONObject jsonObject_M2 = jsonObject_D2.getJSONObject("main");
-                    JSONObject jsonObject_D3 = jsonArray.getJSONObject(3);
-                    JSONObject jsonObject_M3 = jsonObject_D3.getJSONObject("main");
-                    JSONObject jsonObject_D4 = jsonArray.getJSONObject(4);
-                    JSONObject jsonObject_M4 = jsonObject_D4.getJSONObject("main");
-                    JSONObject jsonObject_D5 = jsonArray.getJSONObject(5);
-                    JSONObject jsonObject_M5 = jsonObject_D5.getJSONObject("main");
-                    JSONObject jsonObject_D6 = jsonArray.getJSONObject(6);
-                    JSONObject jsonObject_M6 = jsonObject_D6.getJSONObject("main");
-                    JSONObject jsonObject_D7 = jsonArray.getJSONObject(7);
-                    JSONObject jsonObject_M7 = jsonObject_D7.getJSONObject("main");
-                    JSONObject jsonObject_D8 = jsonArray.getJSONObject(8);
-                    JSONObject jsonObject_M8 = jsonObject_D8.getJSONObject("main");
+                    Log.e("school_edu", school_code[0]);
+                    Log.e("school_code", school_code[1]);
 
-                    double[] tempmax = new double[9];
-                    tempmax[0] = jsonObject_M0.getDouble("temp_max")-273.15;
-                    tempmax[1] = jsonObject_M1.getDouble("temp_max")-273.15;
-                    tempmax[2] = jsonObject_M2.getDouble("temp_max")-273.15;
-                    tempmax[3] = jsonObject_M3.getDouble("temp_max")-273.15;
-                    tempmax[4] = jsonObject_M4.getDouble("temp_max")-273.15;
-                    tempmax[5] = jsonObject_M5.getDouble("temp_max")-273.15;
-                    tempmax[6] = jsonObject_M6.getDouble("temp_max")-273.15;
-                    tempmax[7] = jsonObject_M7.getDouble("temp_max")-273.15;
-                    tempmax[8] = jsonObject_M8.getDouble("temp_max")-273.15;
-                    temp_max = tempmax[0];
-                    for(int i = 0; i < tempmax.length; i++) {
-                        if(temp_max < tempmax[i]) {
-                            temp_max = tempmax[i];
-                        }
-                    }
+                    tvResult2.setText(school_code[0] + " " + school_code[1]);
 
-                    double[] tempmin = new double[9];
-                    tempmin[0] = jsonObject_M0.getDouble("temp_min")-273.15;
-                    tempmin[1] = jsonObject_M1.getDouble("temp_min")-273.15;
-                    tempmin[2] = jsonObject_M2.getDouble("temp_min")-273.15;
-                    tempmin[3] = jsonObject_M3.getDouble("temp_min")-273.15;
-                    tempmin[4] = jsonObject_M4.getDouble("temp_min")-273.15;
-                    tempmin[5] = jsonObject_M5.getDouble("temp_min")-273.15;
-                    tempmin[6] = jsonObject_M6.getDouble("temp_min")-273.15;
-                    tempmin[7] = jsonObject_M7.getDouble("temp_min")-273.15;
-                    tempmin[8] = jsonObject_M8.getDouble("temp_min")-273.15;
-                    temp_min = tempmin[0];
-                    for(int i = 0; i < tempmin.length; i++) {
-                        if(temp_min > tempmin[i]) {
-                            temp_min = tempmin[i];
-                        }
-                    }
-
-                    int humidity0 = jsonObject_M0.getInt("humidity");
-                    int humidity1 = jsonObject_M1.getInt("humidity");
-                    int humidity2 = jsonObject_M2.getInt("humidity");
-                    int humidity3 = jsonObject_M3.getInt("humidity");
-                    int humidity4 = jsonObject_M4.getInt("humidity");
-                    int humidity5 = jsonObject_M5.getInt("humidity");
-                    int humidity6 = jsonObject_M6.getInt("humidity");
-                    int humidity7 = jsonObject_M7.getInt("humidity");
-                    int humidity8 = jsonObject_M8.getInt("humidity");
-                    humidity = (humidity0 + humidity1 + humidity2 + humidity3 + humidity4 + humidity5 + humidity6 + humidity7 + humidity8)/9;
-
-                    JSONObject jsonObjectClouds0 = jsonObject_D0.getJSONObject("clouds");
-                    int clouds0 = jsonObjectClouds0.getInt("all");
-                    JSONObject jsonObjectClouds1 = jsonObject_D1.getJSONObject("clouds");
-                    int clouds1 = jsonObjectClouds1.getInt("all");
-                    JSONObject jsonObjectClouds2 = jsonObject_D2.getJSONObject("clouds");
-                    int clouds2 = jsonObjectClouds2.getInt("all");
-                    JSONObject jsonObjectClouds3 = jsonObject_D3.getJSONObject("clouds");
-                    int clouds3 = jsonObjectClouds3.getInt("all");
-                    JSONObject jsonObjectClouds4 = jsonObject_D4.getJSONObject("clouds");
-                    int clouds4 = jsonObjectClouds4.getInt("all");
-                    JSONObject jsonObjectClouds5 = jsonObject_D5.getJSONObject("clouds");
-                    int clouds5 = jsonObjectClouds5.getInt("all");
-                    JSONObject jsonObjectClouds6 = jsonObject_D6.getJSONObject("clouds");
-                    int clouds6 = jsonObjectClouds6.getInt("all");
-                    JSONObject jsonObjectClouds7 = jsonObject_D7.getJSONObject("clouds");
-                    int clouds7 = jsonObjectClouds7.getInt("all");
-                    JSONObject jsonObjectClouds8 = jsonObject_D8.getJSONObject("clouds");
-                    int clouds8 = jsonObjectClouds8.getInt("all");
-                    clouds = (clouds0 + clouds1 + clouds2 + clouds3 + clouds4 + clouds5 + clouds6 + clouds7 + clouds8)/9;
+                    getStudentDish(school_code[0], school_code[1]);
 
                 }catch(JSONException e) {
                     e.printStackTrace();
@@ -229,6 +175,7 @@ public class DishFragment extends Fragment {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(getContext());
         requestQueue.add(now_stringRequest);
-        requestQueue.add(stringRequest);
+//        requestQueue.add(stringRequest);
+        return school_code;
     }
 }
