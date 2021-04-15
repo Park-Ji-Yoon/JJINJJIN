d3fe6d96a371b8c95ce307c01a4dc4905d0ab7d7
Park-Ji-Yoon <purun030515@gmail.com>
2021-04-15 21:22:18 +0900

    급식 api 연동, 학교/교육청 코드 api 연동

--- app/src/main/java/com/example/jjinjjin/DishFragment.java
+++ app/src/main/java/com/example/jjinjjin/DishFragment.java
@@ -1,11 +1,6 @@
 package com.example.jjinjjin;
 
-import android.app.DownloadManager;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -26,6 +24,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class DishFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
@@ -34,22 +37,19 @@ public class DishFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    TextView tvResult1;
-    TextView tvResult2;
+    TextView tvDate;
+    TextView tvSchoolName;
+    TextView cardView01;
+    TextView cardView02;
+    TextView cardView03;
     View view;
     Button btnSchoolName;
     EditText edSchoolName;
 
-    public DishFragment() {
-    }
+    String today;
+    String tomorrow;
 
-    public static DishFragment newInstance(String param1, String param2) {
-        DishFragment fragment = new DishFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public DishFragment() {
     }
 
     @Override
@@ -65,19 +65,32 @@ public class DishFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_dish, container, false);
-        tvResult1 = view.findViewById(R.id.tvResult1);
-        tvResult2 = view.findViewById(R.id.tvResult2);
-        btnSchoolName = view.findViewById(R.id.btnSchoolname);
-        edSchoolName = view.findViewById(R.id.edSchoolName);
+        tvDate = view.findViewById(R.id.tvDate);
+        tvSchoolName = view.findViewById(R.id.tvSchoolName);
+        cardView01 = view.findViewById(R.id.info_text01);
+        cardView02 = view.findViewById(R.id.info_text02);
+        cardView03 = view.findViewById(R.id.info_text03);
+//        tvDish = view.findViewById(R.id.tvDish);
+//        btnSchoolName = view.findViewById(R.id.btnSchoolname);
+//        edSchoolName = view.findViewById(R.id.edSchoolName);
 
-        btnSchoolName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(edSchoolName.getText() != null) {
-                    getSchoolCode();
-                }
-            }
-        });
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        SimpleDateFormat mFormat = new SimpleDateFormat("yyyyMMdd");
+        today = mFormat.format(cal.getTime());
+        cal.add(Calendar.DATE, 1);
+        tomorrow = mFormat.format(cal.getTime());
+
+        getStudentDish("B10", "7010569");
+
+//        btnSchoolName.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(edSchoolName.getText() != null) {
+//                    getSchoolCode();
+//                }
+//            }
+//        });
 
         return view;
     }
@@ -88,34 +101,61 @@ public class DishFragment extends Fragment {
 
         final String[] output = new String[2];
 
+        Log.e("today : ", today);
+        Log.e("tomorrow : ", tomorrow);
+
         nowtempUrl = dish_url + "?Type=" + "json"
                 + "&pIndex=" + "1"
                 + "&pSize=" + "100"
-                + "&ATPT_OFCDC_SC_CODE=" + school_edu
-                + "&SD_SCHUL_CODE=" + school_code
-                + "&KEY=" + "7abd1b38b28943c0a64f7784516d3feb";
+                + "&ATPT_OFCDC_SC_CODE=" + school_code
+                + "&SD_SCHUL_CODE=" + school_edu
+                + "&KEY=" + "7abd1b38b28943c0a64f7784516d3feb"
+                + "&MLSV_FROM_YMD=" + today
+                + "&MLSV_TO_YMD=" + tomorrow;
 
         StringRequest now_stringRequest = new StringRequest(Request.Method.POST, nowtempUrl, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try{
+//                    https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=T10&SD_SCHUL_CODE=9296071&MLSV_FROM_YMD=20210411&MLSV_TO_YMD=20210413
                     JSONObject jsonResponse = new JSONObject(response);
                     JSONArray mealServiceDietInfo = jsonResponse.getJSONArray("mealServiceDietInfo");
                     JSONObject body = mealServiceDietInfo.getJSONObject(1);
                     JSONArray body_array = body.getJSONArray("row");
-                    JSONObject dish = body_array.getJSONObject(0);
-                    String school_name = dish.getString("SCHUL_NM");
-                    String lunch = dish.getString("DDISH_NM");
-                    String[] lunch_arr = lunch.split("<br/>");
+                    JSONObject breakfast = body_array.getJSONObject(0);
+                    JSONObject lunch = body_array.getJSONObject(1);
+                    JSONObject dinner = body_array.getJSONObject(2);
+                    String school_name = breakfast.getString("SCHUL_NM");
+                    String breakfast_menu = breakfast.getString("DDISH_NM");
+                    String lunch_menu = lunch.getString("DDISH_NM");
+                    String dinner_menu = dinner.getString("DDISH_NM");
+                    String[] breakfast_arr = breakfast_menu.split("<br/>");
+                    String[] lunch_arr = lunch_menu.split("<br/>");
+                    String[] dinner_arr = dinner_menu.split("<br/>");
 
-                    Log.e("Object : ", String.valueOf(dish));
-                    Log.e("school name : ", school_name);
-                    Log.e("lunch : ", lunch);
-                    for(int i=0; i<lunch_arr.length; i++){
-                        String imsi = lunch_arr[i];
-                        Log.e("lunch_arr : ", imsi);
+                    Log.e("breakfast_menu : ", breakfast_menu);
+
+                    String breakfast_result = "";
+                    String lunch_result = "";
+                    String dinner_result = "";
+
+                    for(int i=0; i<breakfast_arr.length; i++) {
+                        breakfast_result += breakfast_arr[i] + "\n";
                     }
-                    tvResult1.setText(lunch);
+                    for(int i=0; i<lunch_arr.length; i++) {
+                        lunch_result += lunch_arr[i] + "\n";
+                    }
+                    for(int i=0; i<dinner_arr.length; i++) {
+                        dinner_result += dinner_arr[i] + "\n";
+                    }
+
+                    Date currentTime = Calendar.getInstance().getTime();
+                    String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 급식", Locale.getDefault()).format(currentTime);
+                    tvDate.setText(date_text);
+                    tvSchoolName.setText(school_name);
+                    cardView01.setText(breakfast_result);
+                    cardView02.setText(lunch_result);
+                    cardView03.setText(dinner_result);
 
                 }catch(JSONException e) {
                     e.printStackTrace();
@@ -129,9 +169,7 @@ public class DishFragment extends Fragment {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(getContext());
         requestQueue.add(now_stringRequest);
-//        requestQueue.add(stringRequest);
     }
-
     public String[] getSchoolCode(){
         String nowtempUrl = "";
         String dish_url = "https://open.neis.go.kr/hub/schoolInfo";
@@ -159,7 +197,7 @@ public class DishFragment extends Fragment {
                     Log.e("school_edu", school_code[0]);
                     Log.e("school_code", school_code[1]);
 
-                    tvResult2.setText(school_code[0] + " " + school_code[1]);
+//                    tvResult2.setText(school_code[0] + " " + school_code[1]);
 
                     getStudentDish(school_code[0], school_code[1]);
 
@@ -175,7 +213,6 @@ public class DishFragment extends Fragment {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(getContext());
         requestQueue.add(now_stringRequest);
-//        requestQueue.add(stringRequest);
         return school_code;
     }
 }
--- app/src/main/java/com/example/jjinjjin/ScheduleFragment.java
+++ app/src/main/java/com/example/jjinjjin/ScheduleFragment.java
@@ -2,11 +2,26 @@ package com.example.jjinjjin;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +34,9 @@ public class ScheduleFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    View view;
+    TextView test;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -56,9 +74,58 @@ public class ScheduleFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        test = view.findViewById(R.id.test);
+
+//        isSchool("미림여자정보과학고등학교");
+        isSchool("구라학굥");
+
         return inflater.inflate(R.layout.fragment_schedule, container, false);
+    }
+    public void isSchool(String schoolName){
+        String url = "";
+        String school_url = "https://open.neis.go.kr/hub/schoolInfo";
+        final String[] school_code = {""};
+
+        url = school_url + "?Type=" + "json"
+                + "&pIndex=" + "1"
+                + "&pSize=" + "100"
+                + "&KEY=" + "7c47d7824e0d4a0eb908d2c186de56f1"
+                + "&SCHUL_NM=" + schoolName;
+
+        StringRequest now_stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Nullable
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray mealServiceDietInfo = jsonResponse.getJSONArray("schoolInfo");
+                    JSONObject body = mealServiceDietInfo.getJSONObject(1);
+                    JSONArray body_array = body.getJSONArray("row");
+                    JSONObject dish = body_array.getJSONObject(0);
+                    school_code[0] = dish.getString("SD_SCHUL_CODE");
+                    Log.e("test", school_code[0]); // 테스트 코드
+                    if(school_code[0] != ""){
+                        // ---------------setText("")넣는 부분 ---------------------
+                        test.setText("존재하는 학교입니다");
+                        Log.e("존재O", school_code[0]);
+                    }else{
+                        test.setText("없는 학교입니다");
+                        Log.e("존재X", school_code[0]);
+                    }
+                }catch(JSONException e) {
+                    Log.e("JSONException - 존재X", school_code[0]);
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(now_stringRequest);
     }
 }
