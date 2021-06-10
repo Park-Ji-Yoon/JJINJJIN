1df7cbc6cc3771c886950506ac447512eba3efde
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-06-11 01:29:41 +0900

    현재 위치 날씨 제공

--- app/src/main/java/com/example/jjinjjin/ScheduleFragment.java
+++ app/src/main/java/com/example/jjinjjin/ScheduleFragment.java
@@ -1,87 +1,88 @@
-package com.example.jjinjjin;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import androidx.fragment.app.Fragment;
-
-import com.applikeysolutions.cosmocalendar.view.CalendarView;
-
-import java.util.Calendar;
-import java.util.HashSet;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ScheduleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ScheduleFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    View view;
-    TextView test;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ScheduleFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScheduleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ScheduleFragment newInstance(String param1, String param2) {
-        ScheduleFragment fragment = new ScheduleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_schedule, container, false);
-        CalendarView calendarView = view.findViewById(R.id.calendar_view);
-        Button button;
-        String startDate, endDate;
-
-        //Set First day of the week
-        calendarView.setFirstDayOfWeek(Calendar.MONDAY);
-
-        //Set Orientation 0 = Horizontal | 1 = Vertical
-        calendarView.setCalendarOrientation(0);
-
-        calendarView.setWeekendDays(new HashSet(){{
-            add(Calendar.SATURDAY);
-            add(Calendar.SUNDAY);
-        }});
-
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
-    }
-}
+//package com.example.jjinjjin;
+//
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.CalendarView;
+//import android.widget.TextView;
+//
+//import androidx.fragment.app.Fragment;
+//
+//import com.applikeysolutions.cosmocalendar.view.CalendarView;
+//
+//import java.util.Calendar;
+//import java.util.HashSet;
+//
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link ScheduleFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
+//public class ScheduleFragment extends Fragment {
+//
+//    // TODO: Rename parameter arguments, choose names that match
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    View view;
+//    TextView test;
+//
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+//
+//    public ScheduleFragment() {
+//        // Required empty public constructor
+//    }
+//
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param param2 Parameter 2.
+//     * @return A new instance of fragment ScheduleFragment.
+//     */
+//    // TODO: Rename and change types and number of parameters
+//    public static ScheduleFragment newInstance(String param1, String param2) {
+//        ScheduleFragment fragment = new ScheduleFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        view = inflater.inflate(R.layout.fragment_schedule, container, false);
+//        CalendarView calendarView = view.findViewById(R.id.calendar_view);
+//        Button button;
+//        String startDate, endDate;
+//
+//        //Set First day of the week
+//        calendarView.setFirstDayOfWeek(Calendar.MONDAY);
+//
+//        //Set Orientation 0 = Horizontal | 1 = Vertical
+//        calendarView.setCalendarOrientation(0);
+//
+//        calendarView.setWeekendDays(new HashSet(){{
+//            add(Calendar.SATURDAY);
+//            add(Calendar.SUNDAY);
+//        }});
+//
+//        return inflater.inflate(R.layout.fragment_schedule, container, false);
+//    }
+//}
